90e28baf01558262d956955f5c1d02e3f78970ae
AntoineYANG <814602569@qq.com>
2019-07-04 16:30:24 +0800

    20190704YZD[2]

---
+++ javascripts/firm.js
@@ -0,0 +1,59 @@
+/*
+ * @Author: Antoine YANG 
+ * @Date: 2019-07-04 14:33:05 
+ * @Last Modified by: Antoine YANG
+ * @Last Modified time: 2019-07-04 16:29:52
+ */
+
+(function () {
+    // 资产负债表信息
+    Balance = (function () {
+        function Balance(info) {
+            this.Code = info.SECURITY_CODE;                 // 证券代码
+            this.Name = info.SECURITY_NAME;                 // 证券简称
+            this.CurrentAssets = info.BAME00840M;           // 流动资产
+            this.FixedAssets = info.BAME01320M;             // 固定资产
+            this.OtherAssets = info.BAME01330M;             // 其他资产
+            this.TotalAssets = info.BAME01340M;             // 资产总计
+            this.CurrentLiability = info.BAME01980M;        // 流动负债
+            this.FixedLiability = info.BAME02190M;          // 固定负债
+            this.OtherLiability = info.BAME02200M;          // 其他负债
+            this.TotalLiability = info.BAME02210M;          // 负债总计
+            this.TotalEquity = info.BAME02470M;             // 所有者权益总计
+            this.TotalLiability_Equity = info.BAME02480M;   // 负债和所有者权益总计
+        }
+        return Balance;
+    })();
+    window.Balance = Balance;
+
+    // 利润表信息
+    Income = (function () {
+        function Income(info) {
+            this.Code = info.SECURITY_CODE;                 // 证券代码
+            this.Name = info.SECURITY_NAME;                 // 证券简称
+            this.TotalRevenue = info.PRME00010M;            // 营业总收入
+            this.TotalCost = info.PRME00500M;               // 营业总成本
+            this.OperationProfit = info.PRME0940M;          // 营业利润
+            this.ProfitBeforeTax = info.PRME1050M;          // 利润总额
+            this.NetProfit = info.PRME01080M;               // 净利润
+            this.ProfitAvailable = info.PRME01170M;         // 税后总额
+            this.TotalIncome = info.PRME01330M;             // 综合收益总额
+            this.ShareRepurchase = info.PRME01370M;         // 每股收益
+        }
+        return Income;
+    })();
+    window.Income = Income;
+    
+    // 利润表信息
+    CashFlow = (function () {
+        function CashFlow(info) {
+            this.Code = info.SECURITY_CODE;                 // 证券代码
+            this.Name = info.SECURITY_NAME;                 // 证券简称
+            this.FromOperation = info.CFME01210M;           // 经营活动产生的现金流量净额
+            this.FromInvestment = info.CFME01610M;          // 投资活动产生的现金流量净额
+            this.FromFund = info.CFME2060M;                 // 筹资活动产生的现金流量净额
+        }
+        return CashFlow;
+    })();
+    window.CashFlow = CashFlow;
+})();
--- javascripts/myjs.js
+++ javascripts/myjs.js
@@ -2,29 +2,85 @@
  * @Author: Antoine YANG 
  * @Date: 2019-07-04 10:56:05 
  * @Last Modified by: Antoine YANG
- * @Last Modified time: 2019-07-04 11:42:11
+ * @Last Modified time: 2019-07-04 16:14:38
  */
 
-var dataset = {
-  "BalanceSheet": [],
-  "IncomeStatement": [],
-  "CashFlowStatement": []
+var dataset = {};
+
+var Sheets = {
+  BalanceSheet: [],
+  IncomeStatement: [],
+  CashFlowStatement: []
 };
 
 var SRC = {
-  "BalanceSheet": "src/BalanceSheet.csv",
-  "IncomeStatement": "src/IncomeStatement.csv",
-  "CashFlowStatement": "src/CashFlowStatement.csv"
+  BalanceSheet: "src/BalanceSheet.csv",
+  IncomeStatement: "src/IncomeStatement.csv",
+  CashFlowStatement: "src/CashFlowStatement.csv"
 };
 
 (function load() {
   var csv = d3.dsv(",", "text/csv;charset=gb2312");
   csv(SRC.BalanceSheet, function (data) {
-    dataset.BalanceSheet = data;
+    Sheets.BalanceSheet = data;
+    for (var i = 0; i < data.length; i++) {
+      var year = (data[i].DATA_YEAR).toString().substring(0, 4);
+      var ctx = "undefined";
+      if (data[i].CONTEXTREF == "合并年初至报告期末" || data[i].CONTEXTREF == "合并上年年初至报告期末")
+        ctx = "Merge";
+      else if (data[i].CONTEXTREF == "母公司年初至报告期末" || data[i].CONTEXTREF == "母公司上年年初至报告期末")
+        ctx = "Parent";
+      if (dataset[year] == void 0)
+        dataset[year] = {};
+      if (dataset[year][ctx] == void 0) {
+        dataset[year][ctx] = {
+          Balance: [],
+          Income: [],
+          CashFlow: []
+        };
+      }
+      dataset[year][ctx].Balance.push(new Balance(data[i]));
+    }
     csv(SRC.IncomeStatement, function (data) {
-      dataset.IncomeStatement = data;
+      Sheets.IncomeStatement = data;
+      for (var i = 0; i < data.length; i++) {
+        var year = (data[i].DATA_YEAR).toString().substring(0, 4);
+        var ctx = "undefined";
+        if (data[i].CONTEXTREF == "合并年初至报告期末" || data[i].CONTEXTREF == "合并上年年初至报告期末")
+          ctx = "Merge";
+        else if (data[i].CONTEXTREF == "母公司年初至报告期末" || data[i].CONTEXTREF == "母公司上年年初至报告期末")
+          ctx = "Parent";
+        if (dataset[year] == void 0)
+          dataset[year] = {};
+        if (dataset[year][ctx] == void 0) {
+          dataset[year][ctx] = {
+            Balance: [],
+            Income: [],
+            CashFlow: []
+          };
+        }
+        dataset[year][ctx].Income.push(new Income(data[i]));
+      }
       csv(SRC.CashFlowStatement, function (data) {
-        dataset.CashFlowStatement = data;
+        Sheets.CashFlowStatement = data;
+        for (var i = 0; i < data.length; i++) {
+          var year = (data[i].DATA_YEAR).toString().substring(0, 4);
+          var ctx = "undefined";
+          if (data[i].CONTEXTREF == "合并年初至报告期末" || data[i].CONTEXTREF == "合并上年年初至报告期末")
+            ctx = "Merge";
+          else if (data[i].CONTEXTREF == "母公司年初至报告期末" || data[i].CONTEXTREF == "母公司上年年初至报告期末")
+            ctx = "Parent";
+          if (dataset[year] == void 0)
+            dataset[year] = {};
+          if (dataset[year][ctx] == void 0) {
+            dataset[year][ctx] = {
+              Balance: [],
+              Income: [],
+              CashFlow: []
+            };
+          }
+          dataset[year][ctx].CashFlow.push(new CashFlow(data[i]));
+        }
         init();
       })
     })
